3f333e19694e55e3a12336a68da9c471522959ce
igorTopko <igor.topko@icloud.com>
2024-06-06 23:14:20 +0300

    homework

---
+++ less01.py
@@ -0,0 +1,14 @@
+number = (input("Введіть 4-х значне число:"))
+
+value_1 = 8
+value_2 = 5
+
+result_too = value_1 % value_2
+result_yoo = value_2 % value_1
+result_boo = value_1 // value_2
+result_goo = value_2 // value_1
+
+print (result_yoo)
+print (result_too)
+print (result_boo)
+print (result_goo)
