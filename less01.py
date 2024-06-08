84c8cd8970390fb63cc8730d05f513accfd35e76
igorTopko <igor.topko@icloud.com>
2024-06-08 13:08:16 +0300

    homework

--- less01.py
+++ less01.py
@@ -1,15 +1,16 @@
-input("Введіть 4-х значне число:")
+value_1 = int(input("Введіть 4-х значне число: "))
 
 
-value_1 = 8
-value_2 = 5
+if value_1 < 1000 or value_1 > 9999:
+    print("Будь ласка, введіть 4-х значне число.")
+else:
+    result_too = value_1 // 1000
+    result_yoo = value_1 % 1000 // 100
+    result_boo = value_1 % 100 // 10
+    result_goo  = value_1 % 10
 
-result_too = value_1 % value_2
-result_yoo = value_2 % value_1
-result_boo = value_1 // value_2
-result_goo = value_2 // value_1
+print(result_too)
+print(result_yoo)
+print(result_boo)
+print(result_goo)
 
-print (result_yoo)
-print (result_too)
-print (result_boo)
-print (result_goo)
